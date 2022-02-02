4642a23843bac078d0d1db903181a67579f7e500
ajay_S <sajayyy01234@gmail.com>
2022-02-02 10:29:03 +0530

    Added mobile view

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
-// import Siema from 'siema';
 
-// new Siema()
+const mySiema = new Siema({
+    selector: ".feature-carousel",
+    loop: true
+});
+
+const prev = document.querySelector('.left-arrow');
+const next = document.querySelector('.right-arrow');
+
+prev.addEventListener('click', () => mySiema.prev());
+next.addEventListener('click', () => mySiema.next());
